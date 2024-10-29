e7acdf01a942aef17b829b65f47d32ffe766e1da
MateusMSoares <mateus.marques100@hotmail.com>
2024-10-28 21:20:57 -0300

    busca em largura

--- Arvore Binaria/main.cpp
+++ Arvore Binaria/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 using namespace std;
 
 struct Node{
@@ -9,29 +10,26 @@ struct Node{
     Node(int novoValor) : valor(novoValor), esquerda(nullptr), direita(nullptr) {} 
 };
 
-void inserir(Node*& raiz, int valorNo){
-    Node* noInserido = new Node(valorNo);
+void inserir(Node*& raiz, Node* noInserido){
     if (!raiz){
-        raiz = noInserido;
-        // cout << "inserido: " << noInserido->valor << endl; 
+        raiz = noInserido; 
         return;
     }
 
-    if (valorNo < raiz->valor){
-        inserir(raiz->esquerda, valorNo);
-    } else if(valorNo > raiz->valor){
-        inserir(raiz->direita, valorNo);
+    if (noInserido->valor < raiz->valor){
+        inserir(raiz->esquerda, noInserido);
+    } else if(noInserido->valor > raiz->valor){
+        inserir(raiz->direita, noInserido);
     }
-
 }
 
-void print(Node* raiz){
+void printMenorMaior(Node* raiz){
     if (!raiz){
         return;
     } 
-    print(raiz->esquerda);
+    printMenorMaior(raiz->esquerda);
     cout << raiz->valor << " ";
-    print(raiz->direita);
+    printMenorMaior(raiz->direita);
 }
 
 void printMaiorMenor(Node* raiz){
@@ -43,40 +41,72 @@ void printMaiorMenor(Node* raiz){
     printMaiorMenor(raiz->esquerda);
 }
 
-Node* buscaPorValor(Node* raiz, int valor){
+Node* buscaPorValor(Node* raiz, int valor, int profundidade){
     if (!raiz){
         return nullptr;
     }
 
     if (raiz->valor == valor){
+        cout << "profundidade: " << profundidade << " ";
         return raiz;
     }
     
-    if(valor < raiz->valor){
-        return buscaPorValor(raiz->esquerda, valor);
+    if(valor < raiz->valor){ 
+        return buscaPorValor(raiz->esquerda, valor, profundidade + 1);
     }else{
-        return buscaPorValor(raiz->direita, valor);
+        return buscaPorValor(raiz->direita, valor, profundidade + 1);
     }
     return nullptr;
 }
 
+void percorreHorizontal(deque<Node*> fila){
+    if (fila.size() < 1){
+        return;   
+    }
+    Node* no = fila.front();
+    cout << no->valor << " ";
+    fila.pop_front();
+    if (no->esquerda) fila.push_back(no->esquerda);
+    if (no->direita) fila.push_back(no->direita);
+    percorreHorizontal(fila);
+}
+
+Node* retirar(Node* raiz){
+    
+}
 
 int main(){
     Node* raiz = nullptr;
-    inserir(raiz, 20);
-    inserir(raiz, 45);
-    inserir(raiz, 50);
-    inserir(raiz, 15);
-    inserir(raiz, 10);
-    inserir(raiz, 18);
-    print(raiz);
+    inserir(raiz, new Node(20));
+    inserir(raiz, new Node(45));
+    inserir(raiz, new Node(50));
+    inserir(raiz, new Node(15));
+    inserir(raiz, new Node(10));
+    inserir(raiz, new Node(9));
+    inserir(raiz, new Node(8));
+    inserir(raiz, new Node(7));
+    inserir(raiz, new Node(18));
+    inserir(raiz, new Node(19));
+    inserir(raiz, new Node(7));
+    inserir(raiz, new Node(55));
+    inserir(raiz, new Node(11));
+    inserir(raiz, new Node(35));
+    inserir(raiz, new Node(1));
+    printMenorMaior(raiz);
     cout << endl;
     printMaiorMenor(raiz);
     cout << endl;
-    Node* encontrado = buscaPorValor(raiz, 70);
+    Node* encontrado = buscaPorValor(raiz, 1, 1);
+
     if (encontrado == nullptr) {
         cout << "NOT FOUND";  
     } 
-    cout << "Encontrado: " << encontrado->valor;
+    cout << "Encontrado: " << encontrado->valor << endl;
+    cout << endl;
+
+    deque<Node*> fila;
+    fila.push_back(raiz);
+    percorreHorizontal(fila);
+
     return 0;
 }
